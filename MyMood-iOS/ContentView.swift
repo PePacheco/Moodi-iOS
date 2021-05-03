18198a72f34c890b501e7876aca944793d5164c7
PePacheco <pedro_pacheco98@outlook.com>
2021-05-03 15:17:08 -0300

    Adding the project to a remote repository

---
+++ MyMood-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMood-iOS
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 03/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMood-iOS/MyMood_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMood_iOSApp.swift
+//  MyMood-iOS
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 03/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MyMood_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
