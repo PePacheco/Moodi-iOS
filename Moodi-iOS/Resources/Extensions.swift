804c4e7d171e1e5aec059348c134ad5545aa23da
lccxm <>
2021-05-13 17:34:27 -0300

    Creating DayCalendar model and added date extension

--- Moodi-iOS/Resources/Extensions.swift
+++ Moodi-iOS/Resources/Extensions.swift
@@ -48,6 +48,14 @@ extension Date {
     var dayBefore: Date {
         return Calendar.current.date(byAdding: .day, value: -1, to: self)!
     }
+    
+    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+         return calendar.dateComponents(Set(components), from: self)
+     }
+
+     func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+         return calendar.component(component, from: self)
+     }
 }
 
 extension View {
--- Moodi-iOS/Views/Statistics Views/CalendarView.swift
+++ Moodi-iOS/Views/Statistics Views/CalendarView.swift
@@ -18,14 +18,18 @@ struct CalendarView<DateView>: View where DateView: View {
     }
 
     var body: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
-            ForEach(months, id: \.self) { month in
-                Section(header: header(for: month)) {
-                    ForEach(days(for: month), id: \.self) { date in
-                        if calendar.isDate(date, equalTo: month, toGranularity: .month) {
-                            content(date).id(date)
-                        } else {
-                            content(date).hidden()
+        
+        VStack {
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
+                ForEach(allTimeDayCalendar, id: \.self) { month in
+                    Section(header: header(for: month[0].day.date)) {
+                        ForEach(month, id: \.self) { date in
+                            if calendar.isDate(date, equalTo: date, toGranularity: .month) {
+                                
+                                content(date).id(date)
+                            } else {
+                                content(date).hidden()
+                            }
                         }
                     }
                 }
@@ -39,6 +43,12 @@ struct CalendarView<DateView>: View where DateView: View {
             matching: DateComponents(day: 1, hour: 0, minute: 0, second: 0)
         )
     }
+    
+    var monthArray = DateManager.shared.newMonthArray(month: 5, year: 2021) // passar valor dinamico
+    var days = DatabaseManager.shared.days
+    lazy var dayCalendar = DateManager.fillMonthArray(month: monthArray, days: days)
+    
+    lazy var allTimeDayCalendar: [[DayCalendar]] = [dayCalendar, dayCalendar, dayCalendar]
 
     private func header(for month: Date) -> some View {
         let component = calendar.component(.month, from: month)
@@ -69,7 +79,7 @@ struct CalendarView<DateView>: View where DateView: View {
 struct CalendarView_Previews: PreviewProvider {
     static var previews: some View {
         CalendarView(interval: .init()) { _ in
-            Text("30")
+            Text("31")
                 .padding(8)
                 .background(Color.blue)
                 .cornerRadius(8)
