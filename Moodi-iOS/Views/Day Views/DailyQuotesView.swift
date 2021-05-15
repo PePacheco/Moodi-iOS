e3c909bb229f11c7ad6ded085888a4927f0ff0b2
Santiago <santiago.dgonzaga@hotmail.com>
2021-05-15 01:22:36 -0300

    Adding translate to tips in spanish

--- Moodi-iOS/Views/Day Views/DailyQuotesView.swift
+++ Moodi-iOS/Views/Day Views/DailyQuotesView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DailyQuotesView: View {
     let screenSize: CGSize
     let yesterdayObjective: String
+    let tips: [String] = ["tip1", "tip2", "tip3", "tip4", "tip5", "tip6", "tip7", "tip8", "tip9", "tip10", "tip11", "tip12", "tip13", "tip14", "tip15", "tip16", "tip17", "tip18", "tip19", "tip20", "tip21", "tip22"]
     
     var body: some View {
         Circle()
@@ -17,7 +18,7 @@ struct DailyQuotesView: View {
             .frame(width: screenSize.width*0.65, height: screenSize.width*0.65)
             .overlay(
                 TabView {
-                    SentenceOfTheDay(sentence: NSLocalizedString("DailyQuotesTipLabel", comment: ""), screenSize: screenSize, text: "Lorem ipsum dolor sit amet, consectetur.Lorem ipsum dolor sit amet, consectetur.")
+                    SentenceOfTheDay(sentence: NSLocalizedString("DailyQuotesTipLabel", comment: ""), screenSize: screenSize, text: NSLocalizedString("tip1", comment: ""))
                     
                     SentenceOfTheDay(sentence: NSLocalizedString("DailyQuotesObjectiveLabel", comment: ""), screenSize: screenSize, text: yesterdayObjective)
                 }
